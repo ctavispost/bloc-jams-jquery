823846e1ce47c60a5b7f69e71a375af281464769
ctavispost <ctavispost@gmail.com>
2019-06-05 20:51:07 -0700

    Fix missed line of code

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -9,6 +9,8 @@
 
     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
     const nextSongIndex = currentSongIndex + 1;
+    if (nextSongIndex >= album.songs.length) { return; }
+    
     const nextSong = album.songs[nextSongIndex];
     player.playPause(nextSong);
   });
